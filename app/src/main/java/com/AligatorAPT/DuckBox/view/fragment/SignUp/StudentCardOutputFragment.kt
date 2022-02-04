34a7852cf7c61e6d79f0f7357240783974e9ff19
jiwoo <hjw6112@gmail.com>
2022-02-04 13:43:37 +0900

    feat: SignUp StudentCardOutputFragment

--- app/src/main/java/com/AligatorAPT/DuckBox/view/fragment/SignUp/StudentCardOutputFragment.kt
+++ app/src/main/java/com/AligatorAPT/DuckBox/view/fragment/SignUp/StudentCardOutputFragment.kt
@@ -6,16 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.AligatorAPT.DuckBox.R
+import com.AligatorAPT.DuckBox.databinding.FragmentEmailBinding
+import com.AligatorAPT.DuckBox.databinding.FragmentStudentCardOutputBinding
+import com.AligatorAPT.DuckBox.view.activity.SignUpActivity
 
 class StudentCardOutputFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentStudentCardOutputBinding? = null
+    private val binding: FragmentStudentCardOutputBinding get() = _binding!!
+
+    private var userName = "홍길동"
+    private var studentId = "201911111"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_student_card_output, container, false)
+        _binding = FragmentStudentCardOutputBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init(){
+        val mActivity = activity as SignUpActivity
+        binding.apply {
+            //이름, 학번 설정
+            setUserName.text = userName
+            setStudentId.text = studentId
+
+            studentCardOutputBtn.setOnClickListener {
+                mActivity.changeFragment(MoreInfoFragment())
+            }
+        }
     }
 }
