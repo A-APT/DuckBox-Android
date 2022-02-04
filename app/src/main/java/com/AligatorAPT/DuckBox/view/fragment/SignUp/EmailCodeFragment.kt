546fc70717f9bbdd332ead80b605c1aa183a64e0
jiwoo <hjw6112@gmail.com>
2022-02-04 13:26:56 +0900

    feat: SignUp EmailCodeFragment

--- app/src/main/java/com/AligatorAPT/DuckBox/view/fragment/SignUp/EmailCodeFragment.kt
+++ app/src/main/java/com/AligatorAPT/DuckBox/view/fragment/SignUp/EmailCodeFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import com.AligatorAPT.DuckBox.R
 import com.AligatorAPT.DuckBox.databinding.FragmentEmailBinding
 import com.AligatorAPT.DuckBox.databinding.FragmentEmailCodeBinding
+import com.AligatorAPT.DuckBox.view.activity.SignUpActivity
 
 class EmailCodeFragment : Fragment() {
     private var _binding: FragmentEmailCodeBinding? = null
     private val binding: FragmentEmailCodeBinding get() = _binding!!
+
+    private var isActivateBtn = false
+    private var correctCode = "123456"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +32,38 @@ class EmailCodeFragment : Fragment() {
         init()
     }
 
-    private fun init(){
+    private fun setIsActivateBtn(){
+        val mActivity = activity as SignUpActivity
+        if(isActivateBtn){
+            binding.emailCodeBtn.setBackgroundColor(ContextCompat.getColor(mActivity, R.color.main))
+        }else{
+            binding.emailCodeBtn.setBackgroundColor(ContextCompat.getColor(mActivity, R.color.darkgray))
+        }
+    }
 
+    private fun init(){
+        val mActivity = activity as SignUpActivity
+        binding.apply {
+            //사용자 이메일 설정
+            userEmail.text = "duckBox@konkuk.ac.kr"
+
+            //입력값 확인
+            setEmailCode.doAfterTextChanged {
+                isActivateBtn = setEmailCode.text.toString() != ""
+                setIsActivateBtn()
+            }
+
+            //제출 버튼 클릭 이벤트
+            emailCodeBtn.setOnClickListener {
+                if(setEmailCode.text.toString() == correctCode && isActivateBtn){
+                    errorEmailCode.visibility = View.INVISIBLE
+                    mActivity.changeFragment(ExplainStudentCardFragment())
+                }else{
+                    isActivateBtn = false
+                    errorEmailCode.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
