937946e39e4f35f5ef84cb3a6d2403e00082324b
Jiwoo Hong <hjw6112@gmail.com>
2022-03-16 15:54:57 +0900

    feat: Model create-email-and-user-model

---
+++ app/src/main/java/com/AligatorAPT/DuckBox/model/EmailModel.kt
@@ -0,0 +1,51 @@
+package com.AligatorAPT.DuckBox.model
+
+import android.util.Log
+import com.AligatorAPT.DuckBox.dto.user.EmailTokenDto
+import com.AligatorAPT.DuckBox.retrofit.RetrofitClient
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class EmailModel {
+    fun generateEmailAuth(_email: String){
+        RetrofitClient.EMAIL_CONTROLLER_SERVICE.generateEmailAuth(_email).enqueue(object:
+            Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                Log.d("Response:: ", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.d("onFailure::", "Failed API call with call: " + call +
+                        " + exception: " + t)
+            }
+        })
+    }
+
+    fun verifyEmailToken(_emailTokenDto: EmailTokenDto): Boolean{
+        var isVerified = false
+
+        RetrofitClient.EMAIL_CONTROLLER_SERVICE.verifyEmailToken(_emailTokenDto).enqueue(object:
+            Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                Log.d("Response:: ", response.body().toString())
+                isVerified = true
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.d("onFailure::", "Failed API call with call: " + call +
+                        " + exception: " + t)
+                isVerified = false
+            }
+        })
+
+        return isVerified
+    }
+}
---
+++ app/src/main/java/com/AligatorAPT/DuckBox/model/UserModel.kt
@@ -0,0 +1,31 @@
+package com.AligatorAPT.DuckBox.model
+
+import android.util.Log
+import com.AligatorAPT.DuckBox.dto.user.RegisterDto
+import com.AligatorAPT.DuckBox.retrofit.RetrofitClient
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class UserModel{
+    fun register( _userInfo: RegisterDto){
+        Log.d("USER", _userInfo.toString())
+        RetrofitClient.USER_CONTROLLER_SERVICE.register(
+            _userInfo
+        ).enqueue(object:
+            Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                Log.d("Response:: ", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.d("onFailure::", "Failed API call with call: " + call +
+                        " + exception: " + t)
+            }
+        })
+    }
+}
