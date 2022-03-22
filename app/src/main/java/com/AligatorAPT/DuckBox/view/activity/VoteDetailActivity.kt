6a892edc95e318366a1cea346722943d20acd76c
mimwin <kms105903@naver.com>
2022-03-22 20:50:54 +0900

    feat: change VoteCreateActivity to CreateVoteActivity

--- app/src/main/java/com/AligatorAPT/DuckBox/view/activity/VoteDetailActivity.kt
+++ app/src/main/java/com/AligatorAPT/DuckBox/view/activity/VoteDetailActivity.kt
@@ -3,10 +3,14 @@ package com.AligatorAPT.DuckBox.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.AligatorAPT.DuckBox.R
+import com.AligatorAPT.DuckBox.databinding.ActivityVoteDetailBinding
 
 class VoteDetailActivity : AppCompatActivity() {
+    lateinit var binding : ActivityVoteDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vote_detail)
+        binding = ActivityVoteDetailBinding.inflate(layoutInflater)
     }
 }
