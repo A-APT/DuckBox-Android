71d74f96fda2b9984f59e12883a69835310154e6
Jiwoo Hong <hjw6112@gmail.com>
2022-03-31 01:14:01 +0900

    fix: Login string-to-RequestBody

--- app/src/main/java/com/AligatorAPT/DuckBox/model/EmailModel.kt
+++ app/src/main/java/com/AligatorAPT/DuckBox/model/EmailModel.kt
@@ -4,52 +4,61 @@ import android.util.Log
 import com.AligatorAPT.DuckBox.dto.user.EmailTokenDto
 import com.AligatorAPT.DuckBox.retrofit.RetrofitClient
 import com.AligatorAPT.DuckBox.retrofit.`interface`.ApiCallback
+import okhttp3.MediaType
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 object EmailModel {
-    fun generateEmailAuth(_email: String, callback: ApiCallback){
-        RetrofitClient.EMAIL_INTERFACE_SERVICE.generateEmailAuth(_email).enqueue(object:
+    fun generateEmailAuth(_email: String, callback: ApiCallback) {
+        //String to text/plain
+        val body: RequestBody = RequestBody.create(MediaType.parse("text/plain"), _email)
+
+        RetrofitClient.EMAIL_INTERFACE_SERVICE.generateEmailAuth(body).enqueue(object :
             Callback<ResponseBody> {
             override fun onResponse(
                 call: Call<ResponseBody>,
                 response: Response<ResponseBody>
             ) {
                 Log.d("Response:: ", response.toString())
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     callback.apiCallback(true)
-                }else{
+                } else {
                     callback.apiCallback(false)
                 }
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.d("onFailure::", "Failed API call with call: " + call +
-                        " + exception: " + t)
+                Log.d(
+                    "onFailure::", "Failed API call with call: " + call +
+                            " + exception: " + t
+                )
             }
         })
     }
 
-    fun verifyEmailToken(_emailTokenDto: EmailTokenDto, callback: ApiCallback){
-        RetrofitClient.EMAIL_INTERFACE_SERVICE.verifyEmailToken(_emailTokenDto).enqueue(object:
+    fun verifyEmailToken(_emailTokenDto: EmailTokenDto, callback: ApiCallback) {
+        RetrofitClient.EMAIL_INTERFACE_SERVICE.verifyEmailToken(_emailTokenDto).enqueue(object :
             Callback<ResponseBody> {
             override fun onResponse(
                 call: Call<ResponseBody>,
                 response: Response<ResponseBody>
             ) {
                 Log.d("Response:: ", response.toString())
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     callback.apiCallback(true)
-                }else{
+                } else {
                     callback.apiCallback(false)
                 }
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.d("onFailure::", "Failed API call with call: " + call +
-                        " + exception: " + t)
+                Log.d(
+                    "onFailure::", "Failed API call with call: " + call +
+                            " + exception: " + t
+                )
                 callback.apiCallback(false)
             }
         })
